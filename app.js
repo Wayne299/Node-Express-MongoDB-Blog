8178c3951fc82bd23199a303092f6ac84ac9d488
Wayne299 <673156454@qq.com>
2019-10-21 22:19:11 +0800

    add markdown edit

--- app.js
+++ app.js
@@ -1,61 +1,62 @@
-var bodyParser       = require("body-parser"),
-	methodOverride   = require("method-override"),
+var bodyParser = require("body-parser"),
+	methodOverride = require("method-override"),
 	expressSanitizer = require("express-sanitizer"),
-	mongoose         = require("mongoose"),
-	//==============user setting=============
-	LocalStrategy    = require("passport-local"),
-	passport         = require("passport"),
-	User             = require("./models/user"),  // 导入User model
-	//=======================================
-	Blog             = require("./models/blog"),
-	express          = require("express"),
-	app              = express();
+	mongoose = require("mongoose"),
+	// ==============user setting=============
+	LocalStrategy = require("passport-local"),
+	passport = require("passport"),
+	User = require("./models/user"), // 导入User model
+	// =======================================
+	Blog = require("./models/blog"),
+	express = require("express"),
+	app = express();
 
 //requiring routes
 var blogsRoutes = require("./routes/blogs"),
 	indexRoutes = require("./routes/index");
 
 //APP CONFIG
-mongoose.connect("mongodb://localhost/blog_app",{ useNewUrlParser: true });
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-app.set("view engine","ejs");
-app.use(express.static("public"));  // ??
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(expressSanitizer()); // 防止用户输入script  
+mongoose.connect("mongodb://localhost/blog_app", { useNewUrlParser: true });
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+app.set("view engine", "ejs");
+app.use(express.static("public")); // ??
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSanitizer()); // 防止用户输入script
 app.use(methodOverride("_method"));
 
 // PASSPORT CONFIGUARTION
-app.use(require("express-session")({
-	secret            : "this is the secret",
-	resave            : false,  // 默认
-	saveUninitialized : false   // 默认
-}));
+app.use(
+	require("express-session")({
+		secret: "this is the secret",
+		resave: false, // 默认
+		saveUninitialized: false // 默认
+	})
+);
 app.use(passport.initialize());
 app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));  // middleware 来自 passportLocalMongoose的method
+passport.use(new LocalStrategy(User.authenticate())); // middleware 来自 passportLocalMongoose的method
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 //RESTFUL ROUTES
 // NAME      PAHT           HTTP VERB     MOOGOOSE METHOD
-// index 	/dogs 		      get 			Dog.find()
-// new 	    /dogs/new 	      get 			N/A
-// create   /dogs	          get 			Dogs.create()
-// show     /dogs/:id         get 			Dog.findById()
-// edit     /dogs/:id/edit 	  get 			Dog.findById()
-// update   /dogs/:id         put 			Dog.findByIdAndUpdate()
-// destroy  /dogs/:id         delete 		Dog.findByIdAndRemove()
+// index 	/blogs 		      get 			Blog.find()
+// new 	    /blogs/new 	      get 			N/A
+// create   /blogs	          get 			Blogs.create()
+// show     /blogs/:id        get 			Blog.findById()
+// edit     /blogs/:id/edit	  get 			Blog.findById()
+// update   /blogs/:id        put 			Blog.findByIdAndUpdate()
+// destroy  /blogs/:id        delete 		Blog.findByIdAndRemove()
 
-app.use(function(req, res, next){
+app.use(function(req, res, next) {
 	res.locals.currentUser = req.user;
 	next();
 }); //全局赋值 currentUser = req.user (id和username)
 
 app.use(indexRoutes);
-app.use("/blogs",blogsRoutes);  //前面自动填 /blogs
+app.use("/blogs", blogsRoutes); //前面自动填 /blogs
 
-
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Server Has Started!");
+app.listen(process.env.PORT || 3000, function() {
+	console.log("Server Has Started!");
 });
--- routes/blogs.js
+++ routes/blogs.js
@@ -1,27 +1,34 @@
 var express = require("express");
-var router  = express.Router({mergeParams: true}); //findById 可以传入/:id
-var Blog    = require("../models/blog");
+var showdown = require("showdown");
+var router = express.Router({ mergeParams: true }); //findById 可以传入/:id
+var Blog = require("../models/blog");
 
 // INDEX ROUTE
-router.get("/",function(req, res){
-	Blog.find({},function(err, blogs){  // blogs 将传入index.ejs
-		if(err){
+router.get("/", function(req, res) {
+	Blog.find({}, function(err, blogs) {
+		// blogs 将传入index.ejs
+		if (err) {
 			console.log("error!");
-		}else{
-			res.render("index",{blogs:blogs, currentUser:req.user}); // 现在登陆的用户名
+		} else {
+			res.render("index", {
+				blogs: blogs,
+				currentUser: req.user,
+				markdown: markdown
+			}); // 现在登陆的用户名
 		}
 	});
 });
 
 //CREATE ROUTE
-router.post("/", isLoggedIn, function(req, res){
+router.post("/", isLoggedIn, function(req, res) {
 	// create blog
 	req.body.blog.body = req.sanitize(req.body.blog.body);
 
-	Blog.create(req.body.blog, function(err, newBlog){ //bodyParser：req.body.blog 自动带着后面的[title] 
-		if(err){
+	Blog.create(req.body.blog, function(err, newBlog) {
+		//bodyParser：req.body.blog 自动带着后面的[title]
+		if (err) {
 			res.render("new");
-		}else{
+		} else {
 			//redirect to the index
 			res.redirect("/blogs");
 		}
@@ -29,61 +36,72 @@ router.post("/", isLoggedIn, function(req, res){
 });
 
 //NEW ROUTE
-router.get("/new", isLoggedIn, function(req, res){
+router.get("/new", isLoggedIn, function(req, res) {
 	res.render("new");
 });
 
 //SHOW ROUTE
-router.get("/:id",function(req,res){
-	Blog.findById(req.params.id, function(err,foundBlog){
-		if(err){
+router.get("/:id", function(req, res) {
+	Blog.findById(req.params.id, function(err, foundBlog) {
+		if (err) {
 			res.redirect("/blogs");
-		}else{
-			res.render("show",{blog:foundBlog});
+		} else {
+			res.render("show", { blog: foundBlog, markdown: markdown });
 		}
 	});
 });
 
 //EDIT ROUTE (new + show)
-router.get("/:id/edit",function(req, res){
-	Blog.findById(req.params.id, function(err, foundBlog){
-		if(err){
+router.get("/:id/edit", function(req, res) {
+	Blog.findById(req.params.id, function(err, foundBlog) {
+		if (err) {
 			res.redirect("/blogs");
-		}else{
-			res.render("edit",{blog:foundBlog})
+		} else {
+			res.render("edit", { blog: foundBlog });
 		}
 	});
 });
 
 //UPDATE ROUTE
-router.put("/:id",function(req, res){
+router.put("/:id", function(req, res) {
 	req.body.blog.body = req.sanitize(req.body.blog.body);
-	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err,updateBlog){ // id, newData, callback
-		if(err){
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(
+		err,
+		updateBlog
+	) {
+		// id, newData, callback
+		if (err) {
 			res.redirect("/blogs");
-		}else{
+		} else {
 			res.redirect("/blogs/" + req.params.id);
 		}
-	});  
+	});
 });
 
 //DELETE ROUTE
-router.delete("/:id",function(req, res){
-	Blog.findByIdAndRemove(req.params.id, function(err){
-		if(err){
+router.delete("/:id", function(req, res) {
+	Blog.findByIdAndRemove(req.params.id, function(err) {
+		if (err) {
 			res.redirect("/blogs");
-		}else{
+		} else {
 			res.redirect("/blogs");
 		}
 	});
 });
 
 //middleware
-function isLoggedIn(req, res, next){  //构造middleware
-	if(req.isAuthenticated()){
+function isLoggedIn(req, res, next) {
+	//构造middleware
+	if (req.isAuthenticated()) {
 		return next();
 	}
 	res.redirect("/login");
-};
+}
+
+function markdown(text) {
+	var converter = new showdown.Converter(),
+		html = converter.makeHtml(text);
+	return html;
+}
 
 module.exports = router;
